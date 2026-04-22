64de27a7c449c56ce4ba23f33f3171d5e1d37d7c
durgashankard <ddangi8103@gmail.com>
2026-04-22 16:29:03 +0530

    Event Binding

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,11 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import { DemoClass } from './Class Components/classComponent-Demo.jsx';
+import { EventBind } from './Class Components/EventBinding-Classcomponent.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
     <DemoClass />
+    <EventBind />
   </StrictMode>,
 )
